0ab1abe19155cc9d962beeb12b106b927824d687
Arun kumar <arunkumarjr1999@gmail.com>
2023-12-01 12:47:16 +0530

    Add React router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Container } from "react-bootstrap";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
@@ -9,10 +10,10 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-        <h1>Welcome to E-=Com-Shop</h1>
+         <Outlet/>
         </Container>
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
